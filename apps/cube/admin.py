81449f0cb4fd867de4315642130e004621a6947b
Rantahu <rantahu@gmail.com>
2017-08-28 02:25:30 -0400

    copied frontend of cozy-exchange

--- apps/cube/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from apps.cube.models import Cube
-
-class CubeAdmin(admin.ModelAdmin):
-    list_display = ('title',)
-    search_fields = ['title']
-
-admin.site.register(Cube, CubeAdmin)
