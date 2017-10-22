7c36c40feaf21336be57d2ae644251fb7e7c5ebf
rantahu <rantahu@gmail.com>
2017-10-22 12:00:03 -0400

    removed docker

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 let API_HOST = '';
-if (process.env.NODE_ENV === 'production') API_HOST = 'vataxia.net';
+if (process.env.NODE_ENV === 'production') API_HOST = 'cozy.exchange';
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
