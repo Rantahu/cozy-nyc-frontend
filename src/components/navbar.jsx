4da6a579fa373e6d9dd705dd5b39bdca2834fb2a
rantahu <rantahu@gmail.com>
2017-11-19 00:25:31 -0500

    added cube.svg

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
-import { IndexLink, Link } from 'react-router'
-import { connect } from 'react-redux';
+import React, {Component} from 'react';
+import {IndexLink, Link} from 'react-router'
+import {connect} from 'react-redux';
 
 // import SearchBar from './searchbar';
 
-@connect((state) => ({
-   isAuth: state.auth.authenticated
-}))
+@connect((state) => ({isAuth: state.auth.authenticated}))
 class Navbar extends Component {
 
-   render(){
-      const { isAuth } = this.props;
+  render() {
+    const {isAuth} = this.props;
 
-      return (
+    return (
       <div id="navbar">
-      <span className="dropdown">
-         <button className="dropdown-btn"><IndexLink to="/">[cube]</IndexLink></button>
-         <ul className="dropdown-content">
-           <li><Link to={'/s'}>store</Link></li>
-           <li><Link to={'/about'}>about</Link></li>
-           <li><Link to={'/contact'}>contact</Link></li>
-         </ul>
-         </span>
-         <span id="navbar-auth" className="dropdown">
-         <button className="dropdown-btn">
-            { !isAuth && <Link to={`/login`}>login</Link> }
-            { isAuth && <Link to={`/dashboard`}>account</Link> }
-          </button>
-         </span>
+        <span className="dropdown">
+          <a className="dropdown-btn">
+            <IndexLink to="/">
+              <img id='brand-img' src='/img/cube.svg'/>
+            </IndexLink>
+          </a>
+          <ul className="dropdown-content">
+            <li>
+              <Link to={'/s'}>store</Link>
+            </li>
+            <li>
+              <Link to={'/about'}>about</Link>
+            </li>
+            <li>
+              <Link to={'/contact'}>contact</Link>
+            </li>
+          </ul>
+        </span>
       </div>
-      )
-   }
+    )
+  }
 }
 
 export default Navbar;
