d5b5c548f5dfc4ef38125ee8c152b3455773ee2f
rantahu <rokney@cozy.nyc>
2018-03-16 03:43:28 +0000

    changes for production

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-let API_HOST = '';
-if (process.env.NODE_ENV === 'production') API_HOST = 'api.cozy.nyc';
+let API_HOST = 'api-dev.cozy.nyc';
 
 module.exports = {
     devtool: 'cheap-module-eval-source-map',
-    entry: './src/index',
-    module: {
+    entry: './src/index.jsx',
+    module: { 
         loaders: [
             {
                 test: /\.(js|jsx)$/,
-                loaders: ['babel-loader'],
-                exclude: /node_modules/
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+		query: {
+                   babelrc: false,
+                   presets: ['es2015', 'stage-2', 'react'],
+                   plugins: ['transform-decorators-legacy', 'react-hot-loader/babel']
+                }
             },
             {
-                test: /\.css/,
-                loaders: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ]
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!sass-loader'
             }
         ]
     },
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'js/bundle.min.js'
+        path: path.join(__dirname, '../dist'),
+        filename: 'js/bundle.min.js',
+	chunkFilename: '[name].js'
     },
     resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.jsx']
+    	modules: ['node_modules'],
+   	extensions: ['.js', '.jsx']
     },
     plugins: [
+	new webpack.optimize.UglifyJsPlugin({minimize: true}),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('production'),
-                'API_ROOT': JSON.stringify(`http://${API_HOST}`)
+		 'API_ROOT': JSON.stringify(`http://${API_HOST}`)
             }
         })
     ]
